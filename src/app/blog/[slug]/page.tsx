bb653e474e2393605e91864a96e05f12581094e7
Warisha naz <warishanaz265@gmail.com>
2025-01-04 21:57:48 +0500

    initial commit

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -11,26 +11,26 @@ import CommentBox from "../../../components/CommentBox";
 
 export const revalidate = 10; //seconds
 
-const page = async({ params }: { params: { slug: string } }) => {
+const page = async({ params :{slug} }: { params: { slug: string } }) => {
  
   
-  const query = `*[_type == 'blog' && slug.current=='${params.slug}']{
+  const query = `*[_type == 'blog' && slug.current== "${slug}"]{
   title,summary,image,content,
     author->{name,bio,image}
 }[0]`;
-  const data = await client.fetch(query);
-  console.log(data);
+  const post = await client.fetch(query);
+  console.log(post);
 
   return (
     <article className="mt-12 mb-24 md:px-2 flex flex-col font-serif m-12 gap-y-12">
       {/* Blog Title */}
       <h1 className="md:text-3xl underline font-bold wrapper  text-black dark:text-white ">
-        {data.title}
+        {post.title}
       </h1>
 
       {/* Feature Image */}
       <Image
-        src={urlFor(data.image)}
+        src={urlFor(post.image)}
         alt="author image"
         width={600}
         height={600}
@@ -44,30 +44,30 @@ const page = async({ params }: { params: { slug: string } }) => {
           Summary
         </h2>
         <p className="md:text-lg font-serif leading-relaxed text-justify wrapper text-black/80 dark:text-white/80">
-          {data.summary}
+          {post.summary}
         </p>
       </section>
 
       {/* Author section (Image & bio) */}
       <section className="md:px-2 flex gap-2  items-start">
         <Image
-          src={urlFor(data.author.image)}
+          src={urlFor(post.author.image)}
           alt="author image"
           height={400}
           width={400}
           className="object-cover rounded-full md:h-16 md:w-16 "
         />
         <div className="flex flex-col gap-1">
-          <h3> {data.author.name}</h3>
+          <h3> {post.author.name}</h3>
           <p className="italic font-medium text-md text-black/80 dark:text-white/80">
-            {data.author.bio}
+            {post.author.bio}
           </p>
         </div>
       </section>
 
       {/* Main body of blog */}
       <section className="text-lg leading-normal text-dark/80 dark:text-white/80">
-        <PortableText value={data.content} components={components} />
+        <PortableText value={post.content} components={components} />
       </section>
       <CommentBox />
     </article>
